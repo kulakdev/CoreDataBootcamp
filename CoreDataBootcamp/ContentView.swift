812738c0ca3cb164813337871692af16cc92dde9
gitConstantineKulak <constantinekulak@proton.me>
2023-08-28 13:53:08 +0300

    Decided to finally commit my changes Added CoreData Implemented the creation of todo tasks in a list Implemented the UI from main app

--- CoreDataBootcamp/ContentView.swift
+++ CoreDataBootcamp/ContentView.swift
@@ -12,64 +12,156 @@ struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
     @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
+        entity: TaskEntity.entity(),
+        sortDescriptors: [NSSortDescriptor(keyPath: \TaskEntity.name, ascending: false)]) var tasks: FetchedResults<TaskEntity>
 
+    @State var textFieldText: String = ""
+    @State var isCreatorOpen: Bool = false
+    
+    let myColor = Color(#colorLiteral(red: 0.3098039216, green: 0.7607843137, blue: 0.7882352941, alpha: 1))
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+        ScrollView {
+            NavigationView {
+                
+                ZStack {
+                    VStack(spacing: 20) {
+                        TextField("Create task", text: $textFieldText)
+                            .font(.headline)
+                            .padding(.leading)
+                            .foregroundColor(.black)
+                            .frame(maxWidth: .infinity)
+                            .background(.white)
+                            .cornerRadius(10)
+                            .padding(.horizontal)
+                        
+                        Button(action: {
+                            
+                        },
+                               label: {
+                            Text("Submit")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .frame(maxWidth: .infinity)
+                                .frame(height: 55)
+                                .background(myColor)
+                                .cornerRadius(10)
+                        })
+                        
+                        List {
+                            ForEach(tasks) { task in
+                                HStack {
+                                    Image(systemName: task.completion ? "checkmark.square.fill" : "square")
+                                        .foregroundColor(myColor)
+                                    
+                                    Text(task.name ?? "")
+                                        .font(Font.custom("Poppins-Light", size: 14))
+                                        .lineLimit(1)
+                                    Spacer()
+                                    
+                                }
+                                .frame(maxWidth: .infinity)
+                                .frame(height: 40)
+                                .background(Color.white)
+                                .padding(.horizontal, 20.0)
+                                .onTapGesture {
+                                    task.completion = !task.completion
+                                    saveItems()
+                                }
+                            }
+                            .onDelete(perform: deleteItems)
+                        }
+                        .listStyle(PlainListStyle())
+                        
+                        HStack {
+                            Spacer()
+                            Button {
+                                isCreatorOpen = !isCreatorOpen
+                            } label: {
+                                Image(systemName: "plus")
+                                    .frame(width: 26, height: 26)
+                                    .background(myColor)
+                                    .clipShape(Circle())
+                                    .foregroundColor(Color.white)
+                                    .padding()
+                            }
+                        }
                     }
                 }
-                .onDelete(perform: deleteItems)
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
         }
     }
 
     private func addItem() {
         withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+            let newItem = TaskEntity(context: viewContext)
+            newItem.name = textFieldText
+            saveItems()
+            textFieldText = ""
         }
     }
 
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            guard let index = offsets.first else { return }
+            let taskEntity = tasks[index]
+            viewContext.delete(taskEntity)
+//            offsets.map { tasks[$0] }.forEach(viewContext.delete)
+            saveItems()
+        }
+    }
+    
+    private func saveItems() {
+        do {
+            try viewContext.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+    }
+    
+    @ViewBuilder
+    func CreateTaskOverlayView(isCreatorOpen: Bool) -> some View {
+        ZStack{
+            VStack {
+                HStack {
+                    Text("Create a new task")
+                        .font(.title2)
+                        .foregroundColor(.black)
+                    Spacer()
+                    Button {
+                        self.isCreatorOpen = !isCreatorOpen
+                        textFieldText = ""
+                    } label: {
+                        Image(systemName: "xmark")
+                            .foregroundColor(.black)
+                    }
+                }
+                .background(Color.white)
+                .cornerRadius(20)
+                Spacer()
+                TextField("Enter the task name", text: $textFieldText)
+                    .frame(height: 50)
+                    .padding(5)
+                    .background(Color.black)
+                    .opacity(0.4)
+                    .cornerRadius(10)
+                HStack {
+                    Spacer()
+                    Button {} label: {
+                        Text("Accept")
+                            .padding(7)
+                            .foregroundColor(.white)
+                            .background(Color.accentColor)
+                            .cornerRadius(10)
+                    }
+                }
+                Spacer()
             }
+            .frame(width: 325, height: 150)
+            .padding()
+            .background(Color.white)
+            .cornerRadius(20)
+            
         }
     }
 }
